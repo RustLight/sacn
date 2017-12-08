5085547f92fbd05f2a8146ad052da2b8c5d70606
Lukas Schmierer <lukas.schmierer@lschmierer.de>
2017-12-08 15:33:04 +0100

    pack universe discovery packet

--- src/packet.rs
+++ src/packet.rs
@@ -8,7 +8,7 @@
 #[cfg(feature = "std")]
 use std::vec::Vec;
 
-use byteorder::{ByteOrder, BigEndian};
+use byteorder::{ByteOrder, NetworkEndian};
 use uuid::Uuid;
 use arrayvec::ArrayString;
 
@@ -48,7 +48,7 @@ impl<'a> Protocol for AcnRootLayerProtocol<'a> {
     fn pack(&self, buf: &mut [u8]) {
         assert!(buf.len() >= self.len(), "buffer not large enough");
         // Preamble Size
-        BigEndian::write_u16(&mut buf[0..2], 0x0010);
+        NetworkEndian::write_u16(&mut buf[0..2], 0x0010);
         // Post-amble Size
         zeros(&mut buf[2..4], 2);
         // ACN Packet Identifier
@@ -71,13 +71,14 @@ impl<'a> Protocol for E131RootLayer<'a> {
             match self.data {
                 E131RootLayerData::DataPacket(ref data) => data.len(),
                 E131RootLayerData::SynchronizationPacket(ref data) => data.len(),
+                E131RootLayerData::UniverseDiscoveryPacket(ref data) => data.len(),
             }
     }
 
     fn pack(&self, buf: &mut [u8]) {
         assert!(buf.len() >= self.len(), "buffer not large enough");
         // Length
-        BigEndian::write_u16(&mut buf[0..2], self.len() as u16);
+        NetworkEndian::write_u16(&mut buf[0..2], self.len() as u16);
         buf[0] &= 0x0f;
         // Flags
         buf[0] |= 0x70;
@@ -86,11 +87,12 @@ impl<'a> Protocol for E131RootLayer<'a> {
             // VECTOR_ROOT_E131_DATA
             #[cfg_attr(rustfmt, rustfmt_skip)]
             E131RootLayerData::DataPacket(_) => {
-                BigEndian::write_u32(&mut buf[2..6], 0x00000004)
+                NetworkEndian::write_u32(&mut buf[2..6], 0x00000004)
             }
             // VECTOR_ROOT_E131_EXTENDED
-            E131RootLayerData::SynchronizationPacket(_) => {
-                BigEndian::write_u32(&mut buf[2..6], 0x00000008)
+            E131RootLayerData::SynchronizationPacket(_) |
+            E131RootLayerData::UniverseDiscoveryPacket(_) => {
+                NetworkEndian::write_u32(&mut buf[2..6], 0x00000008)
             }
         }
         // CID
@@ -99,18 +101,20 @@ impl<'a> Protocol for E131RootLayer<'a> {
         match self.data {
             E131RootLayerData::DataPacket(ref data) => data.pack(&mut buf[22..]),
             E131RootLayerData::SynchronizationPacket(ref data) => data.pack(&mut buf[22..]),
+            E131RootLayerData::UniverseDiscoveryPacket(ref data) => data.pack(&mut buf[22..]),
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub enum E131RootLayerData<'a> {
-    DataPacket(E131DataPacketFramingLayer<'a>),
-    SynchronizationPacket(E131SynchronizationPacketFramingLayer),
+    DataPacket(DataPacketFramingLayer<'a>),
+    SynchronizationPacket(SynchronizationPacketFramingLayer),
+    UniverseDiscoveryPacket(UniverseDiscoveryPacketFramingLayer<'a>),
 }
 
 #[derive(Debug, Clone)]
-pub struct E131DataPacketFramingLayer<'a> {
+pub struct DataPacketFramingLayer<'a> {
     pub source_name: ArrayString<[u8; 64]>,
     pub priority: u8,
     pub synchronization_address: u16,
@@ -119,10 +123,10 @@ pub struct E131DataPacketFramingLayer<'a> {
     pub stream_terminated: bool,
     pub force_synchronization: bool,
     pub universe: u16,
-    pub data: E131DataPacketDmpLayer<'a>,
+    pub data: DataPacketDmpLayer<'a>,
 }
 
-impl<'a> Protocol for E131DataPacketFramingLayer<'a> {
+impl<'a> Protocol for DataPacketFramingLayer<'a> {
     fn len(&self) -> usize {
         77 + self.data.len()
     }
@@ -130,19 +134,19 @@ impl<'a> Protocol for E131DataPacketFramingLayer<'a> {
     fn pack(&self, buf: &mut [u8]) {
         assert!(buf.len() >= self.len(), "buffer not large enough");
         // Length
-        BigEndian::write_u16(&mut buf[0..2], self.len() as u16);
+        NetworkEndian::write_u16(&mut buf[0..2], self.len() as u16);
         buf[0] &= 0x0f;
         // Flags
         buf[0] |= 0x70;
         // Vector VECTOR_E131_DATA_PACKET
-        BigEndian::write_u32(&mut buf[2..6], 0x00000002);
+        NetworkEndian::write_u32(&mut buf[2..6], 0x00000002);
         // Source Name
         zeros(&mut buf[6..70], 64);
         buf[6..6 + self.source_name.len()].copy_from_slice(self.source_name.as_bytes());
         // Priority
         buf[70] = self.priority;
         // Synchronization Address
-        BigEndian::write_u16(&mut buf[71..73], self.synchronization_address);
+        NetworkEndian::write_u16(&mut buf[71..73], self.synchronization_address);
         // Sequence Number
         buf[73] = self.sequence_number;
         // Options
@@ -160,7 +164,7 @@ impl<'a> Protocol for E131DataPacketFramingLayer<'a> {
             buf[74] |= 0b00010000
         }
         // Universe
-        BigEndian::write_u16(&mut buf[75..77], self.universe);
+        NetworkEndian::write_u16(&mut buf[75..77], self.universe);
         // Data
         self.data.pack(&mut buf[77..])
     }
@@ -168,11 +172,11 @@ impl<'a> Protocol for E131DataPacketFramingLayer<'a> {
 
 /// Device Management Protocol PDU with SET PROPERTY vector.
 #[derive(Debug, Clone)]
-pub struct E131DataPacketDmpLayer<'a> {
+pub struct DataPacketDmpLayer<'a> {
     pub property_values: &'a [u8],
 }
 
-impl<'a> Protocol for E131DataPacketDmpLayer<'a> {
+impl<'a> Protocol for DataPacketDmpLayer<'a> {
     fn len(&self) -> usize {
         assert!(self.property_values.len() > 0, "DMX START Code required");
         assert!(
@@ -185,7 +189,7 @@ impl<'a> Protocol for E131DataPacketDmpLayer<'a> {
     fn pack(&self, buf: &mut [u8]) {
         assert!(buf.len() >= self.len(), "buffer not large enough");
         // Length
-        BigEndian::write_u16(&mut buf[0..2], self.len() as u16);
+        NetworkEndian::write_u16(&mut buf[0..2], self.len() as u16);
         buf[0] &= 0x0f;
         // Flags
         buf[0] |= 0x70;
@@ -196,21 +200,21 @@ impl<'a> Protocol for E131DataPacketDmpLayer<'a> {
         // First Property Address
         zeros(&mut buf[4..6], 2);
         // Address Increment
-        BigEndian::write_u16(&mut buf[6..8], 0x0001);
+        NetworkEndian::write_u16(&mut buf[6..8], 0x0001);
         // Property value count
-        BigEndian::write_u16(&mut buf[8..10], self.property_values.len() as u16);
+        NetworkEndian::write_u16(&mut buf[8..10], self.property_values.len() as u16);
         // Property values
         buf[10..10 + self.property_values.len()].copy_from_slice(self.property_values);
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct E131SynchronizationPacketFramingLayer {
+pub struct SynchronizationPacketFramingLayer {
     pub sequence_number: u8,
     pub synchronization_address: u16,
 }
 
-impl Protocol for E131SynchronizationPacketFramingLayer {
+impl Protocol for SynchronizationPacketFramingLayer {
     fn len(&self) -> usize {
         11
     }
@@ -218,18 +222,96 @@ impl Protocol for E131SynchronizationPacketFramingLayer {
     fn pack(&self, buf: &mut [u8]) {
         assert!(buf.len() >= self.len(), "buffer not large enough");
         // Length
-        BigEndian::write_u16(&mut buf[0..2], self.len() as u16);
+        NetworkEndian::write_u16(&mut buf[0..2], self.len() as u16);
         buf[0] &= 0x0f;
         // Flags
         buf[0] |= 0x70;
         // Vector VECTOR_E131_EXTENDED_SYNCHRONIZATION
-        BigEndian::write_u32(&mut buf[2..6], 0x00000001);
+        NetworkEndian::write_u32(&mut buf[2..6], 0x00000001);
         // Sequence Number
         buf[6] = self.sequence_number;
         // Synchronization Address
-        BigEndian::write_u16(&mut buf[7..9], self.synchronization_address);
+        NetworkEndian::write_u16(&mut buf[7..9], self.synchronization_address);
         // Reserved
         zeros(&mut buf[9..11], 2);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UniverseDiscoveryPacketFramingLayer<'a> {
+    pub source_name: ArrayString<[u8; 64]>,
+    data: UniverseDiscoveryPacketUniverseDiscoveryLayer<'a>,
+}
+
+impl<'a> Protocol for UniverseDiscoveryPacketFramingLayer<'a> {
+    fn len(&self) -> usize {
+        74 + self.data.len()
+    }
+
+    fn pack(&self, buf: &mut [u8]) {
+        assert!(buf.len() >= self.len(), "buffer not large enough");
+        // Length
+        NetworkEndian::write_u16(&mut buf[0..2], self.len() as u16);
+        buf[0] &= 0x0f;
+        // Flags
+        buf[0] |= 0x70;
+        // Vector VECTOR_E131_EXTENDED_DISCOVERY
+        NetworkEndian::write_u32(&mut buf[2..6], 0x00000002);
+        // Source Name
+        zeros(&mut buf[6..70], 64);
+        buf[6..6 + self.source_name.len()].copy_from_slice(self.source_name.as_bytes());
+        // Reserved
+        zeros(&mut buf[70..74], 4);
+        // Data
+        self.data.pack(&mut buf[74..])
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UniverseDiscoveryPacketUniverseDiscoveryLayer<'a> {
+    pub page: u8,
+    pub last_page: u8,
+    pub universes: &'a [u16],
+}
+
+impl<'a> Protocol for UniverseDiscoveryPacketUniverseDiscoveryLayer<'a> {
+    fn len(&self) -> usize {
+        assert!(
+            self.universes.len() <= 512,
+            "max 512 universes per page allowed"
+        );
+        8 + self.universes.len() * 2
+    }
+
+    fn pack(&self, buf: &mut [u8]) {
+        assert!(buf.len() >= self.len(), "buffer not large enough");
+        // Length
+        NetworkEndian::write_u16(&mut buf[0..2], self.len() as u16);
+        buf[0] &= 0x0f;
+        // Flags
+        buf[0] |= 0x70;
+        // Vector VECTOR_UNIVERSE_DISCOVERY_UNIVERSE_LIST
+        NetworkEndian::write_u32(&mut buf[2..6], 0x00000001);
+        // Page
+        buf[6] = self.page;
+        // Last Page
+        buf[7] = self.last_page;
+        // Universes
+        for i in 1..self.universes.len() {
+            assert!(
+                self.universes[i] != self.universes[i - 1],
+                "univerese at position {} is not unique",
+                i
+            );
+            assert!(
+                self.universes[i] >= self.universes[i - 1],
+                "univereses not sorted"
+            );
+        }
+        NetworkEndian::write_u16_into(
+            &self.universes[..self.universes.len()],
+            &mut buf[8..8 + self.universes.len() * 2],
+        )
     }
 }
 
@@ -261,7 +343,7 @@ mod test {
         // CID
         0xef, 0x07, 0xc8, 0xdd, 0x00, 0x64, 0x44, 0x01, 0xa3, 0xa2,
         0x45, 0x9e, 0xf8, 0xe6, 0x14, 0x3e,
-        // E1.31 Framing Layer
+        // Data Packet Framing Layer
         // Flags and Length
         0x72, 0x58, // 0x57 in E1-31-2016 sample packet (which is wrong)
         // Vector
@@ -349,7 +431,7 @@ mod test {
         // CID
         0xef, 0x07, 0xc8, 0xdd, 0x00, 0x64, 0x44, 0x01, 0xa3, 0xa2,
         0x45, 0x9e, 0xf8, 0xe6, 0x14, 0x3e,
-        // E1.31 Framing Layer
+        // Synchronization Packet Framing Layer
         // Flags and Length
         0x70, 0x0b, // 0x0a in E1-31-2016 sample packet (which is wrong)
         // Vector
@@ -362,12 +444,58 @@ mod test {
         0, 0,
     ];
 
+    #[cfg_attr(rustfmt, rustfmt_skip)]
+    const TEST_UNIVERSE_DISCOVERY_PACKET: &[u8] = &[
+        // Root Layer
+        // Preamble Size
+        0x00, 0x10,
+        // Post-amble Size
+        0x00, 0x00,
+        // ACN Packet Identifier
+        0x41, 0x53, 0x43, 0x2d, 0x45, 0x31, 0x2e, 0x31, 0x37, 0x00,
+        0x00, 0x00,
+        // Flags and Length Protocol
+        0x70, 0x6e,
+        // Vector
+        0x00, 0x00, 0x00, 0x08,
+        // CID
+        0xef, 0x07, 0xc8, 0xdd, 0x00, 0x64, 0x44, 0x01, 0xa3, 0xa2,
+        0x45, 0x9e, 0xf8, 0xe6, 0x14, 0x3e,
+        // E1.31 Framing Layer
+        // Flags and Length
+        0x70, 0x58,
+        // Vector
+        0x00, 0x00, 0x00, 0x02,
+        // Source Name
+        b'S', b'o', b'u', b'r', b'c', b'e', b'_',  b'A', 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0,
+        // Reserved
+        0, 0, 0, 0,
+        // Universe Discovery Layer
+        // Flags and Length
+        0x70, 0x0e,
+        // Vector
+        0x00, 0x00, 0x00, 0x01,
+        // Page
+        1,
+        // Last Page
+        2,
+        // Universes
+        0, 3, 0, 4, 0, 5,
+
+    ];
+
     #[test]
     fn test_pack_data_packet() {
         let packet = AcnRootLayerProtocol {
             pdu: E131RootLayer {
                 cid: Uuid::from_bytes(&TEST_DATA_PACKET[22..38]).unwrap(),
-                data: E131RootLayerData::DataPacket(E131DataPacketFramingLayer {
+                data: E131RootLayerData::DataPacket(DataPacketFramingLayer {
                     source_name: ArrayString::from("Source_A").unwrap(),
                     priority: 100,
                     synchronization_address: 7962,
@@ -376,7 +504,7 @@ mod test {
                     stream_terminated: false,
                     force_synchronization: false,
                     universe: 1,
-                    data: E131DataPacketDmpLayer { property_values: &TEST_DATA_PACKET[125..638] },
+                    data: DataPacketDmpLayer { property_values: &TEST_DATA_PACKET[125..638] },
                 }),
             },
         };
@@ -394,12 +522,10 @@ mod test {
         let packet = AcnRootLayerProtocol {
             pdu: E131RootLayer {
                 cid: Uuid::from_bytes(&TEST_DATA_PACKET[22..38]).unwrap(),
-                data: E131RootLayerData::SynchronizationPacket(
-                    E131SynchronizationPacketFramingLayer {
-                        sequence_number: 0x65,
-                        synchronization_address: 7962,
-                    },
-                ),
+                data: E131RootLayerData::SynchronizationPacket(SynchronizationPacketFramingLayer {
+                    sequence_number: 0x65,
+                    synchronization_address: 7962,
+                }),
             },
         };
 
@@ -409,5 +535,31 @@ mod test {
         packet.pack(&mut buf);
 
         assert_eq!(&buf[..], TEST_SYNCHRONIZATION_PACKET);
+    }
+
+    #[test]
+    fn test_pack_universe_discovery_packet() {
+        let packet = AcnRootLayerProtocol {
+            pdu: E131RootLayer {
+                cid: Uuid::from_bytes(&TEST_DATA_PACKET[22..38]).unwrap(),
+                data: E131RootLayerData::UniverseDiscoveryPacket(
+                    UniverseDiscoveryPacketFramingLayer {
+                        source_name: ArrayString::from("Source_A").unwrap(),
+                        data: UniverseDiscoveryPacketUniverseDiscoveryLayer {
+                            page: 1,
+                            last_page: 2,
+                            universes: &[3, 4, 5],
+                        },
+                    },
+                ),
+            },
+        };
+
+        assert_eq!(packet.len(), TEST_UNIVERSE_DISCOVERY_PACKET.len());
+
+        let mut buf = [0; 126];
+        packet.pack(&mut buf);
+
+        assert_eq!(&buf[..], TEST_UNIVERSE_DISCOVERY_PACKET);
     }
 }
