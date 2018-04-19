6908b1e0d43955a7f777a05084af0011a205f23a
Lukas Schmierer <lukas.schmierer@lschmierer.de>
2018-04-19 21:18:21 +0200

    cleanup error module

--- src/error.rs
+++ src/error.rs
@@ -11,19 +11,13 @@ use core::str::Utf8Error;
 use core::fmt;
 
 #[cfg(feature = "std")]
-use std::error::Error;
+use std::error;
 
-use uuid::ParseError as UUidParseError;
+use uuid;
 
 /// Errors for parsing of sACN network packets.
 #[derive(Debug)]
-pub enum ParseError<'a> {
-    /// Error parsing the received UUID.
-    Uuid(UUidParseError),
-
-    /// Error parsing received UTF8 string.
-    Utf8(Utf8Error),
-
+pub enum ParseError {
     /// Received PDU flags are invalid.
     PduInvalidFlags(u8),
 
@@ -34,92 +28,112 @@ pub enum ParseError<'a> {
     PduInvalidVector(u32),
 
     /// Other invalid data received.
-    InvalidData(&'a str),
+    InvalidData(&'static str),
 
     /// Buffer does not contain enough data.
     NotEnoughData,
+
+    /// Error parsing the received UUID.
+    UuidError(uuid::ParseError),
+
+    /// Error parsing received UTF8 string.
+    Utf8Error(Utf8Error),
 }
 
-impl<'a> fmt::Display for ParseError<'a> {
+impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            ParseError::Uuid(ref err) => write!(f, "UUID parsing error: {}", err),
-            ParseError::Utf8(ref err) => write!(f, "UTF8 error: {}", err),
             ParseError::PduInvalidFlags(flags) => write!(f, "Flags {:#b} are invalid", flags),
             ParseError::PduInvalidLength(len) => write!(f, "Length {} is invalid", len),
             ParseError::PduInvalidVector(vec) => write!(f, "Vector {:#x} not supported", vec),
-            ParseError::InvalidData(ref msg) => write!(f, "Invalid data: {}", msg),
+            ParseError::InvalidData(msg) => write!(f, "Invalid data: {}", msg),
             ParseError::NotEnoughData => write!(f, "Not enough data supplied"),
+            ParseError::UuidError(err) => write!(f, "UUID parsing error: {}", err),
+            ParseError::Utf8Error(err) => write!(f, "UTF8 error: {}", err),
         }
     }
 }
 
 #[cfg(feature = "std")]
-impl<'a> Error for ParseError<'a> {
+impl error::Error for ParseError {
     fn description(&self) -> &str {
         match *self {
-            ParseError::Uuid(ref err) => err.description(),
-            ParseError::Utf8(ref err) => err.description(),
             ParseError::PduInvalidFlags(_) => "PDU invalid flags",
             ParseError::PduInvalidLength(_) => "PDU invalid length",
             ParseError::PduInvalidVector(_) => "PDU vector not supported",
-            ParseError::InvalidData(ref msg) => msg,
+            ParseError::InvalidData(msg) => msg,
             ParseError::NotEnoughData => "Not enough data supplied",
+            ParseError::UuidError(ref err) => err.description(),
+            ParseError::Utf8Error(ref err) => err.description(),
         }
     }
 
-    fn cause(&self) -> Option<&Error> {
+    fn cause(&self) -> Option<&error::Error> {
         match *self {
-            ParseError::Uuid(ref err) => Some(err),
-            ParseError::Utf8(ref err) => Some(err),
+            ParseError::UuidError(ref err) => Some(err),
+            ParseError::Utf8Error(ref err) => Some(err),
             _ => None,
         }
     }
 }
 
-impl<'a> From<UUidParseError> for ParseError<'a> {
-    fn from(err: UUidParseError) -> ParseError<'a> {
-        ParseError::Uuid(err)
+impl From<uuid::ParseError> for ParseError {
+    fn from(err: uuid::ParseError) -> ParseError {
+        ParseError::UuidError(err)
     }
 }
 
-impl<'a> From<Utf8Error> for ParseError<'a> {
-    fn from(err: Utf8Error) -> ParseError<'a> {
-        ParseError::Utf8(err)
+impl From<Utf8Error> for ParseError {
+    fn from(err: Utf8Error) -> ParseError {
+        ParseError::Utf8Error(err)
     }
 }
 
 /// Errors for packing of sACN network packets.
 #[derive(Debug)]
-pub enum PackError<'a> {
+pub enum PackError {
     /// Packet contains invalid data.
-    InvalidData(&'a str),
+    InvalidData(&'static str),
 
     /// Supplied buffer is not large enough.
     BufferNotLargeEnough,
 }
 
-impl<'a> fmt::Display for PackError<'a> {
+impl fmt::Display for PackError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            PackError::InvalidData(ref msg) => write!(f, "Invalid data: {}", msg),
+            PackError::InvalidData(msg) => write!(f, "Invalid data: {}", msg),
             PackError::BufferNotLargeEnough => write!(f, "Supplied buffer is not large enough"),
         }
     }
 }
 
 #[cfg(feature = "std")]
-impl<'a> Error for PackError<'a> {
+impl error::Error for PackError {
     fn description(&self) -> &str {
         match *self {
-            PackError::InvalidData(ref msg) => msg,
+            PackError::InvalidData(msg) => msg,
             PackError::BufferNotLargeEnough => "Supplied buffer is not large enough",
         }
     }
 
-    fn cause(&self) -> Option<&Error> {
+    fn cause(&self) -> Option<&error::Error> {
         match *self {
             _ => None,
         }
+    }
+}
+
+impl From<ParseError> for PackError {
+    fn from(err: ParseError) -> PackError {
+        PackError::InvalidData(match err {
+            ParseError::PduInvalidFlags(_) => "PDU invalid flags",
+            ParseError::PduInvalidLength(_) => "PDU invalid length",
+            ParseError::PduInvalidVector(_) => "PDU vector not supported",
+            ParseError::InvalidData(msg) => msg,
+            ParseError::NotEnoughData => "Not enough data supplied",
+            ParseError::UuidError(_) => "Invalid UUID",
+            ParseError::Utf8Error(_) => "Invalid UTF-8",
+        })
     }
 }
