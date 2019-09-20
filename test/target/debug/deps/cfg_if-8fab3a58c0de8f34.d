c5985eae35d84222293fab9b007e273661851da6
Paul <pl59@st-andrews.ac.uk>
2019-09-20 17:48:14 +0100

    Created test and lib seperation, simple program that spams values on universe 1 multicast

---
+++ test/target/debug/deps/cfg_if-8fab3a58c0de8f34.d
@@ -0,0 +1,5 @@
+C:\Users\paul\Projects\SACN-SH-Project\Test\target\debug\deps\libcfg_if-8fab3a58c0de8f34.rlib: C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.9\src\lib.rs
+
+C:\Users\paul\Projects\SACN-SH-Project\Test\target\debug\deps\cfg_if-8fab3a58c0de8f34.d: C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.9\src\lib.rs
+
+C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.9\src\lib.rs:
---
+++ test/target/debug/deps/test-ccdd8f51ca6857e0.d
@@ -0,0 +1,5 @@
+C:\Users\paul\Projects\SACN-SH-Project\Test\target\debug\deps\test-ccdd8f51ca6857e0.exe: src/main.rs
+
+C:\Users\paul\Projects\SACN-SH-Project\Test\target\debug\deps\test-ccdd8f51ca6857e0.d: src/main.rs
+
+src/main.rs:
