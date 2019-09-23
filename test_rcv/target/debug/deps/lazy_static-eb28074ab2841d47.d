763449bf630a1d0662a081d50bce660475f19bc8
Paul <paul@lancasterzone.com>
2019-09-23 11:23:04 +0100

    Cleaning up repo ready for gitignore

--- test_rcv/target/debug/deps/lazy_static-eb28074ab2841d47.d
+++
@@ -1,6 +0,0 @@
-C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\deps\liblazy_static-eb28074ab2841d47.rlib: C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
-
-C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\deps\lazy_static-eb28074ab2841d47.d: C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
-
-C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs:
-C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs:
--- test_send/target/debug/deps/test-5f6dee625f7e14a7.d
+++
@@ -1,5 +0,0 @@
-E:\Projects\SACN-SH-Project\test_send\target\debug\deps\test-5f6dee625f7e14a7.exe: src/main.rs
-
-E:\Projects\SACN-SH-Project\test_send\target\debug\deps\test-5f6dee625f7e14a7.d: src/main.rs
-
-src/main.rs:
