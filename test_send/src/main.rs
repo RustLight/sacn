f853ba56aa69a206b5861f12e478545e2331d8ee
Paul <paul@lancasterzone.com>
2019-09-23 10:15:58 +0100

    Recieving working on loop back

--- test_send/src/main.rs
+++ test_send/src/main.rs
@@ -7,14 +7,16 @@ use std::{thread, time}; // https://doc.rust-lang.org/std/thread/fn.sleep.html (
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 fn main() {
-    let dmx_source = DmxSource::with_ip("Controller", "192.168.1.6").unwrap();
+    // let dmx_source = DmxSource::with_ip("Controller", "192.168.1.6").unwrap();
+    let dmx_source = DmxSource::with_ip("Controller", "10.0.0.1").unwrap();
 
     dmx_source.terminate_stream(1);
 
-    let wait_time = time::Duration::from_millis(1);
+    let wait_time = time::Duration::from_millis(500);
 
     loop {
         dmx_source.send(1, &[0, 1, 2]);
+        println!("Sent!");
         thread::sleep(wait_time);
     }
     
