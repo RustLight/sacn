a683827c3ff4f34688c4924070ee33412b799f53
Paul <pl59@st-andrews.ac.uk>
2019-09-23 11:14:40 +0100

    Etablished how specifying which interface/network to use currently works

---
+++ test_rcv/target/debug/deps/sacn-3545869a31a57ee1.d
@@ -0,0 +1,9 @@
+C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\deps\libsacn-3545869a31a57ee1.rlib: C:\Users\paul\Projects\SACN-SH-Project\Lib\src\lib.rs C:\Users\paul\Projects\SACN-SH-Project\Lib\src\error.rs C:\Users\paul\Projects\SACN-SH-Project\Lib\src\packet.rs C:\Users\paul\Projects\SACN-SH-Project\Lib\src\source.rs C:\Users\paul\Projects\SACN-SH-Project\Lib\src\recieve.rs
+
+C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\deps\sacn-3545869a31a57ee1.d: C:\Users\paul\Projects\SACN-SH-Project\Lib\src\lib.rs C:\Users\paul\Projects\SACN-SH-Project\Lib\src\error.rs C:\Users\paul\Projects\SACN-SH-Project\Lib\src\packet.rs C:\Users\paul\Projects\SACN-SH-Project\Lib\src\source.rs C:\Users\paul\Projects\SACN-SH-Project\Lib\src\recieve.rs
+
+C:\Users\paul\Projects\SACN-SH-Project\Lib\src\lib.rs:
+C:\Users\paul\Projects\SACN-SH-Project\Lib\src\error.rs:
+C:\Users\paul\Projects\SACN-SH-Project\Lib\src\packet.rs:
+C:\Users\paul\Projects\SACN-SH-Project\Lib\src\source.rs:
+C:\Users\paul\Projects\SACN-SH-Project\Lib\src\recieve.rs:
---
+++ test_rcv/target/debug/deps/socket2-ae90d0e495aabbcb.d
@@ -0,0 +1,9 @@
+C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\deps\libsocket2-ae90d0e495aabbcb.rlib: C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\lib.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\sockaddr.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\socket.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\utils.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\sys\windows.rs
+
+C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\deps\socket2-ae90d0e495aabbcb.d: C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\lib.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\sockaddr.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\socket.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\utils.rs C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\sys\windows.rs
+
+C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\lib.rs:
+C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\sockaddr.rs:
+C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\socket.rs:
+C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\utils.rs:
+C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.3.11\src\sys\windows.rs:
