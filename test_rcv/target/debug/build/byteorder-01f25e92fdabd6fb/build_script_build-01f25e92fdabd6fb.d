07461887980340483ee04db28ad900e838359ba7
Paul <pl59@st-andrews.ac.uk>
2019-09-23 11:14:40 +0100

    Etablished how specifying which interface/network to use currently works

---
+++ test_rcv/target/debug/build/byteorder-01f25e92fdabd6fb/build_script_build-01f25e92fdabd6fb.d
@@ -0,0 +1,5 @@
+C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\build\byteorder-01f25e92fdabd6fb\build_script_build-01f25e92fdabd6fb.exe: C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\byteorder-1.3.2\build.rs
+
+C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\build\byteorder-01f25e92fdabd6fb\build_script_build-01f25e92fdabd6fb.d: C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\byteorder-1.3.2\build.rs
+
+C:\Users\paul\.cargo\registry\src\github.com-1ecc6299db9ec823\byteorder-1.3.2\build.rs:
---
+++ test_rcv/target/debug/deps/test_rcv-02383de4387cb28b.d
@@ -0,0 +1,5 @@
+C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\deps\test_rcv-02383de4387cb28b.exe: src\main.rs
+
+C:\Users\paul\Projects\SACN-SH-Project\test_rcv\target\debug\deps\test_rcv-02383de4387cb28b.d: src\main.rs
+
+src\main.rs:
