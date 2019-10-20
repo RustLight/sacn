56cd9b716ca649db8a3df3ce1f897ff77621129f
Paul <pl59@st-andrews.ac.uk>
2019-10-20 17:43:06 +0100

    Recieving scan compiling

--- testRcv/src/main.rs
+++ testRcv/src/main.rs
@@ -1,13 +1,7 @@
 extern crate sacn;
 use sacn::DmxSource;
-use sacn::recieve::DmxReciever;
-use sacn::recieve::ACN_SDT_MULTICAST_PORT;
-
+use sacn::recieve::{DmxReciever, ACN_SDT_MULTICAST_PORT, RCV_BUF_DEFAULT_SIZE};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-
-/// The default size of the buffer used to recieve E1.31 packets.
-/// 1143 bytes is biggest packet required as per Section 8 of ANSI E1.31-2018, aligned to 64 bit that is 1144 bytes.
-pub const RCV_BUF_DEFAULT_SIZE: usize = 1144;
 
 fn main() {
     let universe: u16 = 1;
@@ -15,7 +9,7 @@ fn main() {
 
     let mut buf = [0u8; RCV_BUF_DEFAULT_SIZE];
 
-    match reciever.recv_blocking(buf) {
+    match reciever.recv_blocking(&mut buf) {
         Ok (pkt) => {
             println!("Packet recieved: {:?}", pkt);
         }
