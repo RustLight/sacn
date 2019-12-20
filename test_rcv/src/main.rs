74804f871ad28de5a8259a5b1f23804722475d2b
Paul <pl59@st-andrews.ac.uk>
2019-12-20 16:19:47 +0000

    Split up receving between different methods for sync, data and discovery types

--- test_rcv/src/main.rs
+++ test_rcv/src/main.rs
@@ -6,18 +6,6 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 fn main() {
     let universe: u16 = 1;
     let reciever = DmxReciever::listen_universe(universe).unwrap();
-
-    let mut buf = [0u8; RCV_BUF_DEFAULT_SIZE];
-
-    while (true) {
-        match reciever.recv_blocking(&mut buf) {
-            Ok (pkt) => {
-                println!("Packet recieved: {:?}", pkt);
-            }
-            Err (err) => {
-                println!("Error recieving packet: {:?}", err);
-            }
-        }
-    }
+    
     
 }
