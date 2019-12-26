d55608ee8bd8ed0312c913460116a3bb5b544d77
Paul <pl59@st-andrews.ac.uk>
2019-12-26 13:32:59 +0000

    Added multiple universes test, failed as not implemented yet

--- test_both/src/main.rs
+++ test_both/src/main.rs
@@ -16,7 +16,7 @@ fn test_send_recv_single_universe(){
     let universe = 1;
 
     let rcv_thread = thread::spawn(move || {
-        let mut dmx_recv = DmxReciever::listen_universe(universe).unwrap();
+        let mut dmx_recv = DmxReciever::listen_universe(vec![universe]).unwrap();
 
         thread_tx.send(Ok(Vec::new()));
 
@@ -50,7 +50,47 @@ fn test_send_recv_single_universe(){
 
 #[test]
 fn test_send_recv_across_universe(){
-    assert!(false); // Not implemented yet
+    let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
+
+    let thread_tx = tx.clone();
+
+    let rcv_thread = thread::spawn(move || {
+        let mut dmx_recv = DmxReciever::listen_universe(vec![2, 3]).unwrap();
+
+        thread_tx.send(Ok(Vec::new())); // Signal that the receiver is ready to receive.
+
+        thread_tx.send(dmx_recv.recv_data_blocking());
+    });
+
+    rx.recv(); // Blocks until the receiver says it is ready. 
+
+    let dmx_source = DmxSource::new("Controller").unwrap();
+
+    let priority = 100;
+
+    assert!(!dmx_source.send_across_universe(&[2, 3], &TEST_DATA_MULTIPLE_UNIVERSE, priority).is_err(), "Failed to send 2 universes synchronised");
+
+    let received_result: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+
+    rcv_thread.join();
+
+    assert!(!received_result.is_err(), "Failed: Error when receving data");
+
+    let received_data: Vec<DMXData> = received_result.unwrap();
+
+    assert_eq!(received_data.len(), 2); // Check 2 universes received as expected.
+
+    let received_universe_1: DMXData = received_data[0].clone();
+
+    assert_eq!(received_universe_1.universe, 2); // Check that the universe received is as expected.
+
+    assert_eq!(received_universe_1.values, TEST_DATA_MULTIPLE_UNIVERSE[..512].to_vec(), "Universe 1 received payload values don't match sent!");
+
+    let received_universe_2: DMXData = received_data[1].clone();
+
+    assert_eq!(received_universe_2.universe, 3); // Check that the universe received is as expected.
+
+    assert_eq!(received_universe_2.values, TEST_DATA_MULTIPLE_UNIVERSE[512..].to_vec(), "Universe 2 received payload values don't match sent!");
 }
 
 const TEST_DATA_SINGLE_UNIVERSE: [u8; 512] = [
