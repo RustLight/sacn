1255182bfa547e97440722fd27d5118be1a9ebf5
Paul <pl59@st-andrews.ac.uk>
2020-01-01 14:17:33 +0000

    Started adding specific points in the list of parts of the standard

--- Lib/tests/both_tests.rs
+++ Lib/tests/both_tests.rs
@@ -17,39 +17,140 @@ use sacn::packet::UNIVERSE_CHANNEL_CAPACITY;
 // - Decided it shouldn't be seperated.
 
 /// ANSI E1.31-2018 Sections with tests that show compliance:
-/// 1.1 Scope - 
-/// 1.2 Overview and Architecture
+/// 1.1 Scope - No specific test
+/// 1.2 Overview and Architecture 
+    /// - Allows transfer of arbitary START code DMX512-A data:
+    /// - DMX data can be synchronized across multiple receivers using universe syncronisation
+    /// - Uses a ACN wrapper meaning it is compatiable with devices following the ANSI E.1.17 [ACN] standard: 
+    /// - Uses UDP as the transport/IP layer protocol:
+    /// - Supports multicast addressing:
+    /// - Supports unicast addressing: 
 /// 1.3 Appropriate Use of This Standard
+    /// - Uses UDP to provide a non-reliable IP transport mechanism
+    /// - Allows multiple senders and receivers
 /// 1.4 Classes of Data Appropriate for Transmission
+    /// - Allows transfer of arbitary START code DMX512-A data:
 /// 1.5 Universe Synchronization
+    /// - Allows synchronisation through the universe synchronisation mechanism:
 /// 1.6 Universe Discovery
-/// 1.7 Compliance
-/// 2 Normative References
+    /// - Allows universe discovery through the universe discovery mechanism
 /// 3 Definitions
+/// 3.5 Source
+    /// - A source is uniquely identified by a number in the header of the packet:
+    /// - A source may send multiple streams of data for different universes:
+    /// - Multiple sources may output data for a given universe:
+/// 3.6 Receiver
+    /// - A receiever may listen on multiple universes
+/// 3.7 Active Data Slots
+    /// - Sources for E1.31 should specify the location and amount of active data slots
+    ///     using the DMP First Property Address and DMP Property Count fields (shown in Table 4-1):
+/// 3.8 E1.31 Data Packet
+    /// - Identified by being transmitted with the VECTOR_E131_DATA_PACKET vector:
+/// 3.9 E.31 Synchronization Packet
+    /// - Contains only universe synchronisation information and no additional data:
+    /// - Identified by being transmitted with the VECTOR_E131_EXTENDED_SYNCHRONIZATION vector:
+/// 3.10 E1.31 Universe Discovery Packet
+    /// - Identified by being transmitted with the VECTOR_E131_EXTENDED_DISCOVERY vector:
 /// 4 Protocol Packet Structure Summary
+    /// - E1.31 components must support the £1.31 Data Packet and E1.31. Universe Discovery Packet:
+    /// - E1.31 components may support the E1.31 synchronization packet:
 /// 4.1 E1.31 Data Packet
+    /// - Data is formatted as specified in Table 4-1 with all fields being correctly populated:
+    /// - Detection of malformed packets:
+    /// - All packet content must be transmitted in network byte order (big endian):
 /// 4.2 E1.31 Synchronization Packet
+    /// - A universe can be used as a synchronisation universe and to transmit data on simultaneously:
+    /// - Packet is formatted as specified in Table 4-2 with all fields being correctly populated:
+    /// - Detection of malformed packets:
+    /// - All packet content must be transmitted in network byte order (big endian):
 /// 4.3 E1.31 Universe Discovery Packet
+    /// - A set of universe discovery packets shall be sent once every E131_UNIVERSE_DISCOVERY_INTERVAL:
+    /// - The list of E1.31 universes must be sorted:
+    /// - The list of universes may includes synchronisation universes:
+    /// - If the list of universes changes within an E131_UNIVERSE_DISCOVERY_INTERVAL a source may send 
+    ///     upto one additional set of packets to update the information:
+    /// - Packet is formatted as specified in Table 4-3 with all fields being correctly populated:
+    /// - Detection of malformed packets:
+    /// - All packet content must be transmitted in network byte order (big endian):
 /// 5 E1.31 use of the ACN Root Layer Protocol
+    /// - All E1.31 packets should use the ACN Root Layer Protocol as defined in ANSI E1.17 [ACN] specifically
+    ///     the fields specified in Table 5-4 which is for E1.31 on UDP.
+    /// - Detection of malformed packets:
 /// 5.1 Preamble Size
+    /// - The preamble size field must be 0x0010:
+    /// - Packets with a different preamble size must be discarded:
+    /// - The preamble (preamble size field, post-amble size field and ACN packet identifier) length must 
+    ///     match the size given in the field (0x10 octets):
 /// 5.2 Post-amble Size
+    /// - There is no post amble for RLP over UDP so the post-amble size field must be 0:
+    /// - E1.31 receivers must discard packets if the post-amble size is not 0x0000. 
 /// 5.3 ACN Packet Identifier
+    /// - The ACN packet identifier must be exactly 0x41 0x53 0x43 0x2d 0x45 0x31 0x2e 0x31 0x37 0x00 0x00 0x00:
+    /// - E1.31 receivers must discard packets if the ACN packet identifier doesn't match above:
 /// 5.4 Flags & Length
+    /// - The PDU length must be encoded in the low 12 bits of the root layer flags and length field:
+    /// - The flags (top 4 bits) must be 0x7:
+    /// - The PDU length is computed started with octet 16 and counting all remaining octets in the packet including
+    ///     all payload:
+    /// - A ful payload data packet should have a length of 638 octets:
+    /// - A synchronisation packet should have a length of 49 octets:
+    /// - A universe discovery packet length should be computed to the end of the list of universes field:
 /// 5.5 Vector
-/// .5.6 CID (Component Identifier)
+    /// The root layer vector must be VECTOR_ROOT_E131_DATA if the packet contains E1.31 data:
+    /// The root layer vector must be VECTOR_ROOT_E131_EXTENDED if the packet is for univers discovery or synchronisation:
+    /// The packet type / root layer vector cannot be both simultaneously:
+    /// Receivers must discard a packet if the vector isn't one of the above:
+/// 5.6 CID (Component Identifier)
+    /// Must be a UUID - a universally unique identifier that is 128 bit number unique across space and time:
+    /// The CID must be compliant with RFC 4122 [UUID]:
+    /// A piece of equipment must maintain the same CID for its entire lifetime: 
+    /// Must be transmitted in network byte order (big endian):
 /// 6 E1.31 Framing Layer Protocol
 /// 6.1 Flags & Length
+    /// - Each framing layer must start with the flags & length field:
+    /// - The field must be 16 bit with the PDU length encoded in the low 12 bits and 0x7 in the top 4 bits:
+    /// - The PDU length must be computed starting with octet 38 and continue through the last octet provided by the underlying layer:
+    /// - An E1.31 Data Packet with full payload must have a length of 638:
+    /// - An E1.31 Universe Discovery Packet must have a length between 120 and 1144 depending on the list of universes:
 /// 6.2 E1.31 Data Packet Framing Layer
+    /// - The packet must be formatted as specified in Table 6-5:
 /// 6.2.1 E1.31 Data Packet: Vector
+    /// - The E1.31 layer vector must be VECTOR_E131_DATA_PACKET for an E1.31 Data Packet
 /// 6.2.2 E1.31 Data Packet: Source Name
+    /// - The source name must be null-terminated:
+    /// - The source name of a component must match the UACN field as specified in EPI 19 [ACN]:
+    /// - The source name may be the same across multiple universes sourced by the same component:
+    /// - The source name should be unique: Left to the implementer / user-configuration
 /// 6.2.3 E1.31 Data Packet: Priority
+    /// - The most recent E1.31 Data Packet from a single source must supersede any previous packet
+    ///     from that source:
+    /// - A receiver may receiver data for the same universe from multiple sources which is distinguished by examining
+    ///     the CID in the packet:
+    /// - The priority field must be in the range 0 to 200
+    /// - Data from sources with a higher priority (e.g. 200 vs 100) will be treated as the defininive data for that universe.
+    /// - If the E1.31 receiver is also doing universe syncronisation then the behaviour is undefined:
 /// 6.2.3.1 Multiple Sources at Highest Priority
+    /// - If there are multiple sources transmitting data at the same highest currently active priority for a given
+    ///     universe then this must be handled:
+    /// - If a receiver is only capable of processing a certain number of sources of data it will encounter a sources exceeded
+    ///     condition when a greater number of sources are present:
 /// 6.2.3.2 Note on Merge and Arbitration Algorithms
+    /// - Allow various merging algorithms for combining data from multiple sources:
 /// 6.2.3.3 Note on Resolution of Sources Exceeded Condition
+    /// - Various possible resolution mechanisms shouldbe possible:
+    /// - Resolution mechanisms are recommended to not generate different results from the same 
+    ///     source combination on different occasions as it can make troubleshooting more difficult:
 /// 6.2.3.4 Requirements for Merging and Arbitrating
+    /// - The ability to merge/arbitrate between multiple sources, the maximum number of sources which
+    ///     can be handled and the algorithm used should all be declared in user documentation for the device: Left to the implementer
 /// 6.2.3.5 Requirements for Sources Exceeded Resolution
+    /// - The resolution behaviour for equipment to resolve a source exceeded condition should be specified in the user documentation:
+    /// - The sources exceeded condition is highly recommended to be easily detected at the device aswell as potentially through the network:
 /// 6.2.3.6 Requirements for Devices with Multiple Operating Modes
+    /// - All different operating modes for a device should be compliant with the standard or or non-compliant configurations should be 
+    ///     clearly declared as such.
 /// 6.2.4 E1.31 Data Packet: Synchronization Address
+    /// 
 /// 6.2.4.1 Synchronization Address Usage in an E1.31 Data Packet
 /// 6.2.5 E1.31 Data Packet: Sequence Number
 /// 6.2.6 E1.31 Data Packet: Options
