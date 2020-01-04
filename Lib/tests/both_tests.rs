2d1f7034598a5fc51cb0e736ada8ed9b202c3044
Paul <pl59@st-andrews.ac.uk>
2020-01-04 19:43:55 +0000

    Added test_send_recv_across_universe_unicast tests, ipv4 and 6 failing

--- Lib/tests/both_tests.rs
+++ Lib/tests/both_tests.rs
@@ -95,7 +95,7 @@ fn test_send_recv_single_universe_unicast_ipv4(){
 
     let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
 
-    let ip: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), ACN_SDT_MULTICAST_PORT + 1);
+    let ip: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), ACN_SDT_MULTICAST_PORT + 1);
     let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
 
     let priority = 100;
@@ -105,6 +105,60 @@ fn test_send_recv_single_universe_unicast_ipv4(){
     let dst_ip: SocketAddr = SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), ACN_SDT_MULTICAST_PORT);
 
     let _ = dmx_source.send(&[universe], &TEST_DATA_SINGLE_UNIVERSE, Some(priority), Some(dst_ip), None).unwrap();
+
+    let received_result: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+
+    rcv_thread.join().unwrap();
+
+    assert!(!received_result.is_err(), "Failed: Error when receving data");
+
+    let received_data: Vec<DMXData> = received_result.unwrap();
+
+    assert_eq!(received_data.len(), 1); // Check only 1 universe received as expected.
+
+    let received_universe: DMXData = received_data[0].clone();
+
+    assert_eq!(received_universe.universe, universe); // Check that the universe received is as expected.
+
+    assert_eq!(received_universe.values, TEST_DATA_SINGLE_UNIVERSE.to_vec(), "Received payload values don't match sent!");
+}
+
+#[test]
+fn test_send_recv_single_universe_multicast_ipv6(){
+    let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
+
+    let thread_tx = tx.clone();
+
+    let universe = 1;
+
+    let rcv_thread = thread::spawn(move || {
+        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT)){
+            Ok(sr) => sr,
+            Err(_) => panic!("Failed to create sacn receiver!")
+        };
+
+        dmx_recv.set_nonblocking(false).unwrap();
+
+        dmx_recv.listen_universes(&[universe]).unwrap();
+
+        thread_tx.send(Ok(Vec::new())).unwrap();
+
+        thread_tx.send(dmx_recv.recv()).unwrap();
+    });
+
+    let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
+
+    // Note: Localhost / loopback doesn't always support IPv6 multicast. Therefore this may have to be modified to select a specific network using the line below
+    // where PUT_IPV6_ADDR_HERE is replaced with the ipv6 address of the interface to use. https://stackoverflow.com/questions/55308730/java-multicasting-how-to-test-on-localhost (04/01/2020)
+    let ip: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT + 1);
+
+    let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
+
+    let priority = 100;
+
+    dmx_source.register_universe(universe);
+
+    let _ = dmx_source.send(&[universe], &TEST_DATA_SINGLE_UNIVERSE, Some(priority), None, None).unwrap();
 
     let received_result: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
 
@@ -174,60 +228,65 @@ fn test_send_recv_single_universe_multicast_ipv4(){
     assert_eq!(received_universe.values, TEST_DATA_SINGLE_UNIVERSE.to_vec(), "Received payload values don't match sent!");
 }
 
+/// Note: this test assumes perfect network conditions (0% reordering, loss, duplication etc.), this should be the case for
+/// the loopback adapter with the low amount of data sent but this may be a possible cause if integration tests fail unexpectedly.
 #[test]
-fn test_send_recv_single_universe_multicast_ipv6(){
+fn test_send_recv_across_universe_multicast_ipv6(){
     let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
 
     let thread_tx = tx.clone();
 
-    let universe = 1;
+    const UNIVERSES: [u16; 2] = [2, 3];
 
     let rcv_thread = thread::spawn(move || {
-        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT)){
+        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT)) {
             Ok(sr) => sr,
             Err(_) => panic!("Failed to create sacn receiver!")
         };
 
         dmx_recv.set_nonblocking(false).unwrap();
 
-        dmx_recv.listen_universes(&[universe]).unwrap();
+        dmx_recv.listen_universes(&UNIVERSES).unwrap();
 
-        thread_tx.send(Ok(Vec::new())).unwrap();
+        thread_tx.send(Ok(Vec::new())).unwrap(); // Signal that the receiver is ready to receive.
 
-        thread_tx.send(dmx_recv.recv()).unwrap();
+        thread_tx.send(dmx_recv.recv()).unwrap(); // Receive the sync packet, the data packets shouldn't have caused .recv to return as forced to wait for sync.
     });
 
     let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
 
-    // Note: Localhost / loopback doesn't always support IPv6 multicast. Therefore this may have to be modified to select a specific network using the line below
-    // where PUT_IPV6_ADDR_HERE is replaced with the ipv6 address of the interface to use.
     let ip: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT + 1);
-
     let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
 
     let priority = 100;
 
-    dmx_source.register_universe(universe);
+    dmx_source.register_universes(&UNIVERSES);
 
-    println!("{:#?}", dmx_source);
+    dmx_source.send(&UNIVERSES, &TEST_DATA_MULTIPLE_UNIVERSE, Some(priority), None, None).unwrap();
 
-    let _ = dmx_source.send(&[universe], &TEST_DATA_SINGLE_UNIVERSE, Some(priority), None, None).unwrap();
-
-    let received_result: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+    let sync_pkt_res: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
 
     rcv_thread.join().unwrap();
 
-    assert!(!received_result.is_err(), "Failed: Error when receving data");
+    assert!(!sync_pkt_res.is_err(), "Failed: Error when receving packets");
 
-    let received_data: Vec<DMXData> = received_result.unwrap();
+    let mut received_data: Vec<DMXData> = sync_pkt_res.unwrap();
 
-    assert_eq!(received_data.len(), 1); // Check only 1 universe received as expected.
+    received_data.sort(); // No guarantee on the ordering of the receieved data so sort it first to allow easier checking.
 
-    let received_universe: DMXData = received_data[0].clone();
+    assert_eq!(received_data.len(), 2); // Check 2 universes received as expected.
 
-    assert_eq!(received_universe.universe, universe); // Check that the universe received is as expected.
+    assert_eq!(received_data[0].universe, 2); // Check that the universe received is as expected.
 
-    assert_eq!(received_universe.values, TEST_DATA_SINGLE_UNIVERSE.to_vec(), "Received payload values don't match sent!");
+    assert_eq!(received_data[0].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[0].values, TEST_DATA_MULTIPLE_UNIVERSE[..UNIVERSE_CHANNEL_CAPACITY].to_vec(), "Universe 1 received payload values don't match sent!");
+
+    assert_eq!(received_data[1].universe, 3); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[1].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[1].values, TEST_DATA_MULTIPLE_UNIVERSE[UNIVERSE_CHANNEL_CAPACITY..].to_vec(), "Universe 2 received payload values don't match sent!");
 }
 
 /// Note: this test assumes perfect network conditions (0% reordering, loss, duplication etc.), this should be the case for
@@ -290,6 +349,133 @@ fn test_send_recv_across_universe_multicast_ipv4(){
 
     assert_eq!(received_data[1].values, TEST_DATA_MULTIPLE_UNIVERSE[UNIVERSE_CHANNEL_CAPACITY..].to_vec(), "Universe 2 received payload values don't match sent!");
 }
+
+/// Note: this test assumes perfect network conditions (0% reordering, loss, duplication etc.), this should be the case for
+/// the loopback adapter with the low amount of data sent but this may be a possible cause if integration tests fail unexpectedly.
+#[test]
+fn test_send_recv_across_universe_unicast_ipv6(){
+    let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
+
+    let thread_tx = tx.clone();
+
+    const UNIVERSES: [u16; 2] = [2, 3];
+
+    let rcv_thread = thread::spawn(move || {
+        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), ACN_SDT_MULTICAST_PORT)) {
+            Ok(sr) => sr,
+            Err(_) => panic!("Failed to create sacn receiver!")
+        };
+
+        dmx_recv.set_nonblocking(false).unwrap();
+
+        dmx_recv.listen_universes(&UNIVERSES).unwrap();
+
+        thread_tx.send(Ok(Vec::new())).unwrap(); // Signal that the receiver is ready to receive.
+
+        thread_tx.send(dmx_recv.recv()).unwrap(); // Receive the sync packet, the data packets shouldn't have caused .recv to return as forced to wait for sync.
+    });
+
+    let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
+
+    let ip: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), ACN_SDT_MULTICAST_PORT + 1);
+    let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
+
+    let priority = 100;
+
+    dmx_source.register_universes(&UNIVERSES);
+
+    let dst_ip: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), ACN_SDT_MULTICAST_PORT);
+
+    let _ = dmx_source.send(&UNIVERSES, &TEST_DATA_MULTIPLE_UNIVERSE, Some(priority), Some(dst_ip), None).unwrap();
+
+    let sync_pkt_res: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+
+    rcv_thread.join().unwrap();
+
+    assert!(!sync_pkt_res.is_err(), "Failed: Error when receving packets");
+
+    let mut received_data: Vec<DMXData> = sync_pkt_res.unwrap();
+
+    received_data.sort(); // No guarantee on the ordering of the receieved data so sort it first to allow easier checking.
+
+    assert_eq!(received_data.len(), 2); // Check 2 universes received as expected.
+
+    assert_eq!(received_data[0].universe, 2); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[0].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[0].values, TEST_DATA_MULTIPLE_UNIVERSE[..UNIVERSE_CHANNEL_CAPACITY].to_vec(), "Universe 1 received payload values don't match sent!");
+
+    assert_eq!(received_data[1].universe, 3); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[1].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[1].values, TEST_DATA_MULTIPLE_UNIVERSE[UNIVERSE_CHANNEL_CAPACITY..].to_vec(), "Universe 2 received payload values don't match sent!");
+}
+
+/// Note: this test assumes perfect network conditions (0% reordering, loss, duplication etc.), this should be the case for
+/// the loopback adapter with the low amount of data sent but this may be a possible cause if integration tests fail unexpectedly.
+#[test]
+fn test_send_recv_across_universe_unicast_ipv4(){
+    let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
+
+    let thread_tx = tx.clone();
+
+    const UNIVERSES: [u16; 2] = [2, 3];
+
+    let rcv_thread = thread::spawn(move || {
+        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), ACN_SDT_MULTICAST_PORT)){
+            Ok(sr) => sr,
+            Err(_) => panic!("Failed to create sacn receiver!")
+        };
+
+        dmx_recv.set_nonblocking(false).unwrap();
+
+        dmx_recv.listen_universes(&UNIVERSES).unwrap();
+
+        thread_tx.send(Ok(Vec::new())).unwrap(); // Signal that the receiver is ready to receive.
+
+        thread_tx.send(dmx_recv.recv()).unwrap(); // Receive the sync packet, the data packets shouldn't have caused .recv to return as forced to wait for sync.
+    });
+
+    let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
+
+    let ip: SocketAddr = SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), ACN_SDT_MULTICAST_PORT + 1);
+    let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
+
+    let priority = 100;
+
+    dmx_source.register_universes(&UNIVERSES);
+
+    let dst_ip: SocketAddr = SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), ACN_SDT_MULTICAST_PORT);
+
+    let _ = dmx_source.send(&UNIVERSES, &TEST_DATA_MULTIPLE_UNIVERSE, Some(priority), Some(dst_ip), None).unwrap();
+
+    let sync_pkt_res: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+
+    rcv_thread.join().unwrap();
+
+    assert!(!sync_pkt_res.is_err(), "Failed: Error when receving packets");
+
+    let mut received_data: Vec<DMXData> = sync_pkt_res.unwrap();
+
+    received_data.sort(); // No guarantee on the ordering of the receieved data so sort it first to allow easier checking.
+
+    assert_eq!(received_data.len(), 2); // Check 2 universes received as expected.
+
+    assert_eq!(received_data[0].universe, 2); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[0].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[0].values, TEST_DATA_MULTIPLE_UNIVERSE[..UNIVERSE_CHANNEL_CAPACITY].to_vec(), "Universe 1 received payload values don't match sent!");
+
+    assert_eq!(received_data[1].universe, 3); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[1].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[1].values, TEST_DATA_MULTIPLE_UNIVERSE[UNIVERSE_CHANNEL_CAPACITY..].to_vec(), "Universe 2 received payload values don't match sent!");
+}
+
 
 const TEST_DATA_SINGLE_UNIVERSE: [u8; 512] = [
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
