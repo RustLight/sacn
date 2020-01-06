e47c584034892b3a6d7a6d75b5104aa5978122a7
Paul Lancaster <paul@lancasterzone.com>
2020-01-06 13:33:24 +0000

    Added multiple receivers test, required specific network setup to test

--- Lib/tests/both_tests.rs
+++ Lib/tests/both_tests.rs
@@ -17,6 +17,12 @@ use sacn::packet::{UNIVERSE_CHANNEL_CAPACITY, ACN_SDT_MULTICAST_PORT};
 // Report: Should start code be seperated out when receiving? Causes input and output to differ and is technically part of another protocol.
 // - Decided it shouldn't be seperated.
 
+/// For some tests to work multiple instances of the protocol must be on the same network with the same port for example to test multiple simultaneous receivers, this means multiple IP's are needed.
+/// This is achieved by assigning multiple static IP's to the test machine and theses IP's are specified below.
+/// Theses must be changed depending on the network that the test machine is on.
+const TEST_NETWORK_INTERFACE_IP_1: &'static str = "192.168.1.10";
+const TEST_NETWORK_INTERFACE_IP_2: &'static str = "192.168.1.9";
+
 #[test]
 fn test_send_recv_single_universe_unicast_ipv6(){
     let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
@@ -79,7 +85,7 @@ fn test_send_recv_single_universe_unicast_ipv4(){
     let universe = 1;
 
     let rcv_thread = thread::spawn(move || {
-        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), ACN_SDT_MULTICAST_PORT)){
+        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(Ipv4Addr::LOCALHOST.into(), ACN_SDT_MULTICAST_PORT)){
             Ok(sr) => sr,
             Err(_) => panic!("Failed to create sacn receiver!")
         };
@@ -95,14 +101,14 @@ fn test_send_recv_single_universe_unicast_ipv4(){
 
     let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
 
-    let ip: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), ACN_SDT_MULTICAST_PORT + 1);
+    let ip: SocketAddr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), ACN_SDT_MULTICAST_PORT + 1);
     let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
 
     let priority = 100;
 
     dmx_source.register_universe(universe);
 
-    let dst_ip: SocketAddr = SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), ACN_SDT_MULTICAST_PORT);
+    let dst_ip: SocketAddr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), ACN_SDT_MULTICAST_PORT);
 
     let _ = dmx_source.send(&[universe], &TEST_DATA_SINGLE_UNIVERSE, Some(priority), Some(dst_ip), None).unwrap();
 
@@ -531,6 +537,257 @@ fn test_send_recv_partial_capacity_universe_multicast_ipv6(){
     assert_eq!(received_universe.values, TEST_DATA_PARTIAL_CAPACITY_UNIVERSE.to_vec(), "Received payload values don't match sent!");
 }
 
+/// 
+#[test]
+fn test_send_recv_single_alternative_startcode_universe_multicast_ipv6(){
+    let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
+
+    let thread_tx = tx.clone();
+
+    let universe = 1;
+
+    let rcv_thread = thread::spawn(move || {
+        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT)){
+            Ok(sr) => sr,
+            Err(_) => panic!("Failed to create sacn receiver!")
+        };
+
+        dmx_recv.set_nonblocking(false).unwrap();
+
+        dmx_recv.listen_universes(&[universe]).unwrap();
+
+        thread_tx.send(Ok(Vec::new())).unwrap();
+
+        thread_tx.send(dmx_recv.recv()).unwrap();
+    });
+
+    let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
+
+    // Note: Localhost / loopback doesn't always support IPv6 multicast. Therefore this may have to be modified to select a specific network using the line below
+    // where PUT_IPV6_ADDR_HERE is replaced with the ipv6 address of the interface to use. https://stackoverflow.com/questions/55308730/java-multicasting-how-to-test-on-localhost (04/01/2020)
+    let ip: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT + 1);
+
+    let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
+
+    let priority = 100;
+
+    dmx_source.register_universe(universe);
+
+    let _ = dmx_source.send(&[universe], &TEST_DATA_SINGLE_ALTERNATIVE_STARTCODE_UNIVERSE, Some(priority), None, None).unwrap();
+
+    let received_result: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+
+    rcv_thread.join().unwrap();
+
+    assert!(!received_result.is_err(), "Failed: Error when receving data");
+
+    let received_data: Vec<DMXData> = received_result.unwrap();
+
+    assert_eq!(received_data.len(), 1); // Check only 1 universe received as expected.
+
+    let received_universe: DMXData = received_data[0].clone();
+
+    assert_eq!(received_universe.universe, universe); // Check that the universe received is as expected.
+
+    assert_eq!(received_universe.values, TEST_DATA_SINGLE_ALTERNATIVE_STARTCODE_UNIVERSE.to_vec(), "Received payload values don't match sent!");
+}
+
+#[test]
+fn test_send_recv_across_alternative_startcode_universe_multicast_ipv6(){
+    let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
+
+    let thread_tx = tx.clone();
+
+    const UNIVERSES: [u16; 2] = [2, 3];
+
+    let rcv_thread = thread::spawn(move || {
+        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT)) {
+            Ok(sr) => sr,
+            Err(_) => panic!("Failed to create sacn receiver!")
+        };
+
+        dmx_recv.set_nonblocking(false).unwrap();
+
+        dmx_recv.listen_universes(&UNIVERSES).unwrap();
+
+        thread_tx.send(Ok(Vec::new())).unwrap(); // Signal that the receiver is ready to receive.
+
+        thread_tx.send(dmx_recv.recv()).unwrap(); // Receive the sync packet, the data packets shouldn't have caused .recv to return as forced to wait for sync.
+    });
+
+    let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
+
+    let ip: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT + 1);
+    let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
+
+    let priority = 100;
+
+    dmx_source.register_universes(&UNIVERSES);
+
+    dmx_source.send(&UNIVERSES, &TEST_DATA_MULTIPLE_ALTERNATIVE_STARTCODE_UNIVERSE, Some(priority), None, None).unwrap();
+
+    let sync_pkt_res: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+
+    rcv_thread.join().unwrap();
+
+    assert!(!sync_pkt_res.is_err(), "Failed: Error when receving packets");
+
+    let mut received_data: Vec<DMXData> = sync_pkt_res.unwrap();
+
+    received_data.sort(); // No guarantee on the ordering of the receieved data so sort it first to allow easier checking.
+
+    assert_eq!(received_data.len(), 2); // Check 2 universes received as expected.
+
+    assert_eq!(received_data[0].universe, 2); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[0].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[0].values, TEST_DATA_MULTIPLE_ALTERNATIVE_STARTCODE_UNIVERSE[..UNIVERSE_CHANNEL_CAPACITY].to_vec(), "Universe 1 received payload values don't match sent!");
+
+    assert_eq!(received_data[1].universe, 3); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[1].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[1].values, TEST_DATA_MULTIPLE_ALTERNATIVE_STARTCODE_UNIVERSE[UNIVERSE_CHANNEL_CAPACITY..].to_vec(), "Universe 2 received payload values don't match sent!");
+}
+
+/// Note: this test assumes perfect network conditions (0% reordering, loss, duplication etc.), this should be the case for
+/// the loopback adapter with the low amount of data sent but this may be a possible cause if integration tests fail unexpectedly.
+#[test]
+fn test_send_recv_full_capacity_across_universe_multicast_ipv6(){
+    let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
+
+    let thread_tx = tx.clone();
+
+    const UNIVERSES: [u16; 2] = [2, 3];
+
+    let rcv_thread = thread::spawn(move || {
+        let mut dmx_recv = match SacnReceiver::with_ip(SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT)) {
+            Ok(sr) => sr,
+            Err(_) => panic!("Failed to create sacn receiver!")
+        };
+
+        dmx_recv.set_nonblocking(false).unwrap();
+
+        dmx_recv.listen_universes(&UNIVERSES).unwrap();
+
+        thread_tx.send(Ok(Vec::new())).unwrap(); // Signal that the receiver is ready to receive.
+
+        thread_tx.send(dmx_recv.recv()).unwrap(); // Receive the sync packet, the data packets shouldn't have caused .recv to return as forced to wait for sync.
+    });
+
+    let _ = rx.recv().unwrap(); // Blocks until the receiver says it is ready. 
+
+    let ip: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT + 1);
+    let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
+
+    let priority = 100;
+
+    dmx_source.register_universes(&UNIVERSES);
+
+    dmx_source.send(&UNIVERSES, &TEST_DATA_FULL_CAPACITY_MULTIPLE_UNIVERSE, Some(priority), None, None).unwrap();
+
+    let sync_pkt_res: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+
+    rcv_thread.join().unwrap();
+
+    assert!(!sync_pkt_res.is_err(), "Failed: Error when receving packets");
+
+    let mut received_data: Vec<DMXData> = sync_pkt_res.unwrap();
+
+    received_data.sort(); // No guarantee on the ordering of the receieved data so sort it first to allow easier checking.
+
+    assert_eq!(received_data.len(), 2); // Check 2 universes received as expected.
+
+    assert_eq!(received_data[0].universe, 2); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[0].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[0].values, TEST_DATA_FULL_CAPACITY_MULTIPLE_UNIVERSE[..UNIVERSE_CHANNEL_CAPACITY].to_vec(), "Universe 1 received payload values don't match sent!");
+
+    assert_eq!(received_data[1].universe, 3); // Check that the universe received is as expected.
+
+    assert_eq!(received_data[1].sync_uni, 2); // Check that the sync universe is as expected.
+
+    assert_eq!(received_data[1].values, TEST_DATA_FULL_CAPACITY_MULTIPLE_UNIVERSE[UNIVERSE_CHANNEL_CAPACITY..].to_vec(), "Universe 2 received payload values don't match sent!");
+}
+
+// Note: For this test to work the PC must be capable of connecting to the network on 2 IP's, this was done in windows by adding another static IP so the PC was connecting through
+// 2 different IP's to the network. Theses IPs are manually specified as the constants TEST_NETWORK_INTERFACE_IP_1 and TEST_NETWORK_INTERFACE_IP_2 in this test and so to run it must be changed
+// depending on the environment.
+#[test]
+fn test_send_single_universe_multiple_receivers_multicast_ipv4(){
+    let (tx, rx): (Sender<Result<Vec<DMXData>, Error>>, Receiver<Result<Vec<DMXData>, Error>>) = mpsc::channel();
+
+    let thread1_tx = tx.clone();
+    let thread2_tx = tx.clone();
+
+    let universe = 1;
+
+    let rcv_thread1 = thread::spawn(move || {
+        let mut dmx_recv = SacnReceiver::with_ip(SocketAddr::new(IpAddr::V4(TEST_NETWORK_INTERFACE_IP_1.parse().unwrap()), ACN_SDT_MULTICAST_PORT)).unwrap();
+
+        dmx_recv.set_nonblocking(false).unwrap();
+
+        dmx_recv.listen_universes(&[universe]).unwrap();
+
+        thread1_tx.send(Ok(Vec::new())).unwrap();
+
+        thread1_tx.send(dmx_recv.recv()).unwrap();
+    });
+
+    let rcv_thread2 = thread::spawn(move || {
+        let mut dmx_recv = SacnReceiver::with_ip(SocketAddr::new(IpAddr::V4(TEST_NETWORK_INTERFACE_IP_2.parse().unwrap()), ACN_SDT_MULTICAST_PORT)).unwrap();
+
+        dmx_recv.set_nonblocking(false).unwrap();
+
+        dmx_recv.listen_universes(&[universe]).unwrap();
+
+        thread2_tx.send(Ok(Vec::new())).unwrap();
+
+        thread2_tx.send(dmx_recv.recv()).unwrap();
+    });
+
+    let _ = rx.recv().unwrap(); // Blocks until both receivers say they are ready.
+    let _ = rx.recv().unwrap();
+
+    // Note: Localhost / loopback doesn't always support IPv6 multicast. Therefore this may have to be modified to select a specific network using the line below
+    // where PUT_IPV6_ADDR_HERE is replaced with the ipv6 address of the interface to use. https://stackoverflow.com/questions/55308730/java-multicasting-how-to-test-on-localhost (04/01/2020)
+    let ip: SocketAddr = SocketAddr::new(IpAddr::V4(TEST_NETWORK_INTERFACE_IP_1.parse().unwrap()), ACN_SDT_MULTICAST_PORT + 1);
+
+    let mut dmx_source = DmxSource::with_ip("Source", ip).unwrap();
+
+    let priority = 100;
+
+    dmx_source.register_universe(universe);
+
+    let _ = dmx_source.send(&[universe], &TEST_DATA_SINGLE_UNIVERSE, Some(priority), None, None).unwrap();
+
+    let received_result1: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+    let received_result2: Result<Vec<DMXData>, Error> = rx.recv().unwrap();
+
+    rcv_thread1.join().unwrap();
+    rcv_thread2.join().unwrap();
+
+    assert!(!received_result1.is_err(), "Failed: Error when receving data");
+    let received_data1: Vec<DMXData> = received_result1.unwrap();
+    assert_eq!(received_data1.len(), 1); // Check only 1 universe received as expected.
+    let received_universe1: DMXData = received_data1[0].clone();
+    assert_eq!(received_universe1.universe, universe); // Check that the universe received is as expected.
+    assert_eq!(received_universe1.values, TEST_DATA_SINGLE_UNIVERSE.to_vec(), "Received payload values don't match sent!");
+
+    assert!(!received_result2.is_err(), "Failed: Error when receving data");
+    let received_data2: Vec<DMXData> = received_result2.unwrap();
+    assert_eq!(received_data2.len(), 1); // Check only 1 universe received as expected.
+    let received_universe2: DMXData = received_data2[0].clone();
+    assert_eq!(received_universe2.universe, universe); // Check that the universe received is as expected.
+    assert_eq!(received_universe2.values, TEST_DATA_SINGLE_UNIVERSE.to_vec(), "Received payload values don't match sent!");
+}
+
+fn test_send_multiple_receivers_sync_multicast_ipv6(){
+
+}
+
 const TEST_DATA_PARTIAL_CAPACITY_UNIVERSE: [u8; 313] = [0,
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -713,4 +970,69 @@ const TEST_DATA_MULTIPLE_UNIVERSE: [u8; 714] = [0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+    ];
+const TEST_DATA_FULL_CAPACITY_MULTIPLE_UNIVERSE: [u8; 1026] = [0,
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+        0,
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100,
+
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
     ];
