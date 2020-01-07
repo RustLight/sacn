c2966c91dc00295715f5662110d5f9065385c72e
Paul Lancaster <paul@lancasterzone.com>
2020-01-07 20:09:39 +0000

    Test template for multiple senders

--- Lib/tests/both_tests.rs
+++ Lib/tests/both_tests.rs
@@ -891,6 +891,10 @@ fn test_send_recv_across_universe_unicast_ipv4(){
     assert_eq!(received_data[1].values, TEST_DATA_MULTIPLE_UNIVERSE[UNIVERSE_CHANNEL_CAPACITY..].to_vec(), "Universe 2 received payload values don't match sent!");
 }
 
+#[test]
+fn test_two_senders__one_recv_different_universes_multicast_ipv4(){
+    assert!(false, "Test not implemented");
+}
 
 const TEST_DATA_PARTIAL_CAPACITY_UNIVERSE: [u8; 313] = [0,
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
