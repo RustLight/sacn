0fbb76088292429bb68b1d0c273e91cd2a8cc79b
Lan2u <pl59@st-andrews.ac.uk>
2020-02-07 17:59:20 +0000

    Adding cmd line args to the testign

--- script-testing/testing.sh
+++ script-testing/testing.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Run the demo_src, this script is based on the scripts provided as part of the 2018/19 CS3102 Practical 1
+# This script is based on the scripts provided as part of the 2018/19 CS3102 Practical 1
+
+SRC_OUTPUT_PATH=$1
+RCV_OUTPUT_PATH=$2
+SRC_TEST_INPUT=$3
+RCV_TEST_INPUT=$4
+
 
 # host where server is running
 REMOTE_PC=pc5-007-l
@@ -13,13 +19,17 @@ SERVER_HOST=klovia.cs.st-andrews.ac.uk
 # the current directory;
 CURRENT_DIR=$(pwd)
 
-# i) login and run server in the background
+# i) Startup a sender on the first remote pc
 echo "Running src at ${REMOTE_PC}"
-ssh -n -f ${REMOTE_PC} "sh -c 'cd ${CURRENT_DIR}; nohup ./src.sh > /dev/null 2>&1'"
+ssh -n -f ${REMOTE_PC} "sh -c 'cd ${CURRENT_DIR}; nohup ./src.sh > ${SRC_OUTPUT_PATH} < ${SRC_TEST_INPUT}"
 
-# ii) run client playing back
+# ii) Startup a receiver on the second remote pc
 echo "Running rcv at ${REMOTE_PC_2}"
-ssh -n -f ${REMOTE_PC_2} "sh -c 'cd ${CURRENT_DIR}; nohup ./rcv.sh > /dev/null 2>&1 &'"
+ssh -n -f ${REMOTE_PC_2} "sh -c 'cd ${CURRENT_DIR}; nohup ./rcv.sh > ${RCV_OUTPUT_PATH} < ${RCV_TEST_INPUT}"
+
+# iii) Wait to allow both processes the chance to run, this has no specific guarantee that they will 
+#	as it depends on scheduling but it is hoped this will be long enough.
+sleep 10
 
 # Kill stuff running on both PC's
 ssh ${REMOTE_PC} fuser -k -n udp ${PORT}
