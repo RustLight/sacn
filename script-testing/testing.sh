19dc25a765505bae38a6311e8d81362f1a39d404
Lan2u <pl59@st-andrews.ac.uk>
2020-02-09 12:32:40 +0000

    Test 1 setup and passing

--- script-testing/testing.sh
+++ script-testing/testing.sh
@@ -19,17 +19,20 @@ PORT=5568
 # the current directory;
 CURRENT_DIR=$(pwd)
 
-# i) Startup a sender on the first remote pc
-echo "Running src at ${REMOTE_PC}"
-ssh -n -f ${REMOTE_PC} "sh -c 'cd ${CURRENT_DIR}; nohup ./src.sh > ${SRC_OUTPUT_PATH} < ${SRC_TEST_INPUT}'"
-
-# ii) Startup a receiver on the second remote pc
+# Startup a receiver on the second remote pc
 echo "Running rcv at ${REMOTE_PC_2}"
 ssh -n -f ${REMOTE_PC_2} "sh -c 'cd ${CURRENT_DIR}; nohup ./rcv.sh > ${RCV_OUTPUT_PATH} < ${RCV_TEST_INPUT}'"
 
-# iii) Wait to allow both processes the chance to run, this has no specific guarantee that they will 
+# Give the receiver a chance to startup.
+sleep 5
+
+# Startup a sender on the first remote pc
+echo "Running src at ${REMOTE_PC}"
+ssh -n -f ${REMOTE_PC} "sh -c 'cd ${CURRENT_DIR}; nohup ./src.sh > ${SRC_OUTPUT_PATH} < ${SRC_TEST_INPUT}'"
+
+# Wait to allow both processes the chance to run, this has no specific guarantee that they will 
 #	as it depends on scheduling but it is hoped this will be long enough.
-sleep 10
+sleep 5
 
 # Kill stuff running on both PC's
 ssh ${REMOTE_PC} fuser -k -n udp ${PORT}
