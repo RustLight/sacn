56e1b6b663cd785a35de9b3317b4e968b9841b65
Lan2u <pl59@st-andrews.ac.uk>
2020-02-09 14:23:16 +0000

    Code tidy ups, discovered sources timeout - untested

---
+++ script-testing/test.sh
@@ -0,0 +1,1 @@
+sh testing.sh 1 src-out.temp rcv-out.temp test-1-single-universe-data/test1_src test-1-single-universe-data/test1_rcv test-1-single-universe-data/src_expected test-1-single-universe-data/rcv_expected
