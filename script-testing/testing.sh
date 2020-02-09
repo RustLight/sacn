18d50adf9c02a41e527e2784fefeba3fb4ff2c87
Lan2u <pl59@st-andrews.ac.uk>
2020-02-09 15:49:40 +0000

    Added should fail test for sanity checking tester, test 1 and 2 pass, sometimes it seems test 1 randomly fails, added more delay to help with this

--- script-testing/testing.sh
+++ script-testing/testing.sh
@@ -21,36 +21,36 @@ CURRENT_DIR=$(pwd)
 
 # Startup a receiver on the second remote pc
 echo "Running rcv at ${REMOTE_PC_2}"
-ssh -n -f ${REMOTE_PC_2} "sh -c 'cd ${CURRENT_DIR}; nohup ./rcv.sh > ${RCV_OUTPUT_PATH} < ${RCV_TEST_INPUT}'"
+ssh -n -f ${REMOTE_PC_2} "sh -c 'cd ${CURRENT_DIR}; nohup ./rcv.sh > ${RCV_OUTPUT_PATH} < ${RCV_TEST_INPUT} 2>/dev/null'"
 
 # Give the receiver a chance to startup.
-sleep 5
+sleep 1
 
 # Startup a sender on the first remote pc
 echo "Running src at ${REMOTE_PC}"
-ssh -n -f ${REMOTE_PC} "sh -c 'cd ${CURRENT_DIR}; nohup ./src.sh > ${SRC_OUTPUT_PATH} < ${SRC_TEST_INPUT}'"
+ssh -n -f ${REMOTE_PC} "sh -c 'cd ${CURRENT_DIR}; nohup ./src.sh > ${SRC_OUTPUT_PATH} < ${SRC_TEST_INPUT} 2>/dev/null'"
 
 # Wait to allow both processes the chance to run, this has no specific guarantee that they will 
 #	as it depends on scheduling but it is hoped this will be long enough.
-sleep 5
+sleep 1
 
 # Kill stuff running on both PC's
 ssh ${REMOTE_PC} fuser -k -n udp ${PORT}
 ssh ${REMOTE_PC_2} fuser -k -n udp ${PORT}
 
+# Give time for both processes to be killed properly.
+sleep 2
+
 # Check if the output matched the expected, diff will output only if they are different
 # https://stackoverflow.com/questions/12137431/test-if-a-command-outputs-an-empty-string (09/02/2020)
-if [[ $(diff -q ${SRC_OUTPUT_PATH} ${SRC_EXPECTED_OUTPUT}) ]];
+if [[ $(diff -q -N ${SRC_OUTPUT_PATH} ${SRC_EXPECTED_OUTPUT}) ]];
 then
   echo "Test ${TEST_NUM}: FAILED"
 else
-	if [[ $(diff -q ${RCV_OUTPUT_PATH} ${RCV_EXPECTED_OUTPUT}) ]];
+	if [[ $(diff -q -N ${RCV_OUTPUT_PATH} ${RCV_EXPECTED_OUTPUT}) ]];
 	then
 	  echo "Test ${TEST_NUM}: FAILED"
 	else
 	  echo "Test ${TEST_NUM}: PASSED"
 	fi
 fi
-
-# TODO, Delete temporary test output
- 
