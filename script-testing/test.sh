91e538a2a5bdc53a9e77128d8fd88d05fa4e29c7
Lan2u <pl59@st-andrews.ac.uk>
2020-02-09 15:49:40 +0000

    Added should fail test for sanity checking tester, test 1 and 2 pass, sometimes it seems test 1 randomly fails, added more delay to help with this

--- script-testing/test.sh
+++ script-testing/test.sh
@@ -1,2 +1,3 @@
+sh testing.sh 0 src-out.temp rcv-out.temp test-0-should-fail/src test-0-should-fail/rcv test-0-should-fail/src_expected test-0-should-fail/rcv_expected
 sh testing.sh 1 src-out.temp rcv-out.temp test-1-single-universe-data/test1_src test-1-single-universe-data/test1_rcv test-1-single-universe-data/src_expected test-1-single-universe-data/rcv_expected
 sh testing.sh 2 src-out.temp rcv-out.temp test-2-two-uni-sync/src test-2-two-uni-sync/rcv test-2-two-uni-sync/src_expected test-2-two-uni-sync/rcv_expected
