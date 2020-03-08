d6fbf0e97899e61bd3f1d682f0c91e95e9d96059
Lan2u <pl59@st-andrews.ac.uk>
2020-03-08 18:01:01 +0000

    lib.rs file cleaned, documented

--- Lib/src/lib.rs
+++ Lib/src/lib.rs
@@ -8,37 +8,43 @@
 //! Implementation of sACN network protocol.
 //!
 //! This crate implements the Streaming ACN (sACN) network protocol
-//! as specified in ANSI E1.31-2016.
+//! as specified in ANSI E1.31-2018.
 //! Streaming ACN is built on top of and is compatible with the ACN
 //! protocol suite (ANSI E1.17-2015).
+//! 
+//! This file was modified as part of a University of St Andrews Computer Science BSC Senior Honours Dissertation Project.
 
-#![cfg_attr(not(feature = "std"), no_std)]
 #![doc(html_root_url = "https://docs.rs/sacn/")]
 #![warn(missing_docs)]
 
+
 // Recursion limit for error_chain.
 #![recursion_limit="1024"]
+
+/// Error-chain is used for errors within the library to allow chaining errors together to provide more informative backtraces.
+/// This completely replaces the old error system (sACN crate version 0.4.4) which relied on a simple Enum model without proper backtraces.
 #[macro_use]
 extern crate error_chain;
+pub mod error;
 
-#[cfg(feature = "std")]
+/// The library is built on top of socket2 and net2 to provide the underlying UDP networking interface.
+extern crate socket2;
+extern crate net2;
+
+/// The core crate is used for string processing during packet parsing/packing.
 extern crate core;
 
+/// The byteorder crate is used for marshalling data on/off the network in Network Byte Order.
 extern crate byteorder;
-#[cfg(not(feature = "std"))]
-extern crate heapless;
-#[cfg(feature = "std")]
-extern crate net2;
+
+/// The uuid crate is used for working with/generating UUIDs which sACN uses as part of the cid field in the protocol.
 extern crate uuid;
 
-#[cfg(feature = "std")]
-pub use self::source::SacnSource;
-
-pub mod error;
+/// The packet module handles the sACN packets including parsing/packing and sACN related constants.
 pub mod packet;
-#[cfg(feature = "std")]
-mod source;
 
-extern crate lazy_static;
-extern crate socket2;
+/// The source module handles generation of sACN on the network.
+pub mod source;
+
+/// The receive module handles the receiving of sACN on the network.
 pub mod recieve;
