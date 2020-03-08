e9bbcad768713cd3713b8bd81f725382a33700b6
Lan2u <pl59@st-andrews.ac.uk>
2020-03-08 16:20:25 +0000

    Added error-chain to demo_src

--- demo_src/src/error.rs
+++ demo_src/src/error.rs
@@ -1,13 +1,12 @@
-#![recursion_limit="1024"]
-#[macro_use]
-extern crate error_chain;
-
 /// Use the error-chain system to allow handling IO and sACN errors chained together.
 pub mod errors {
     error_chain! {
         foreign_links {
-            Sacn(Sacn::error::errors);
-            Io(::std::io::Error);      
+            Sacn(::sacn::error::errors::Error);
+            Io(::std::io::Error);
         }
+        
+        errors {}
     }
 }
+
