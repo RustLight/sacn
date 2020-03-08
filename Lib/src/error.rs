ec128ba3776807070a1cef8d5f9a06a92e0045af
Lan2u <pl59@st-andrews.ac.uk>
2020-03-08 17:46:39 +0000

    Error is tidied, documented

--- Lib/src/error.rs
+++ Lib/src/error.rs
@@ -1,11 +1,11 @@
-// Copyright 2018 sacn Developers
+// Copyright 2020 sacn Developers
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
-
-//! Errors used through the sacn crate.
+//
+// This file was created as part of a University of St Andrews Computer Science BSC Senior Honours Dissertation Project.
 
 #![warn(missing_docs)]
 
@@ -114,13 +114,6 @@ pub mod errors {
                 description("Error parsing received UTF8 string"),
                 display("Error parsing received UTF8 string, msg: {}", msg)
             }
-
-            // /// An action wasn't able to complete within the given time and timed out.
-            // /// 
-            // /// This was created rather than just the io WouldBlock or TimedOut as both the errors are the same 
-            // TimedOut(msg: String) {
-
-            // }
         }
     }
 }
