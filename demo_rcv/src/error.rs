ce543c2a8218b077fe8f5da996d046fbaeff68ca
Lan2u <pl59@st-andrews.ac.uk>
2020-03-09 17:58:03 +0000

    Added license/Uni attribute headers to files

--- demo_rcv/src/error.rs
+++ demo_rcv/src/error.rs
@@ -1,3 +1,12 @@
+// Copyright 2020 sacn Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+// This file was created as part of a University of St Andrews Computer Science BSC Senior Honours Dissertation Project.
+
 /// Use the error-chain system to allow handling IO and sACN errors chained together.
 /// error_chain! macro automatically creates the Error / ErrorKind / Result required to use the Errors/external errors below with error-chain.
 pub mod errors {
