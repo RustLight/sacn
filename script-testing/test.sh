7a305f2fb2dcccd9e85888fa1a31711c0c65c7a9
Lan2u <pl59@st-andrews.ac.uk>
2020-03-09 18:22:01 +0000

    Added SSH overflow-sequence test, found bug, fixed, tests pass

--- script-testing/test.sh
+++ script-testing/test.sh
@@ -1,4 +1,5 @@
 sh testing.sh 0-Should-Fail src-out.temp rcv-out.temp test-0-should-fail/src test-0-should-fail/rcv test-0-should-fail/src_expected test-0-should-fail/rcv_expected 2
-sh testing.sh 1-Single-Uni-Data src-out.temp rcv-out.temp test-1-single-universe-data/test1_src test-1-single-universe-data/test1_rcv test-1-single-universe-data/src_expected test-1-single-universe-data/rcv_expected 2
+sh testing.sh 1-Single-Uni-Data 1src-out.temp 1rcv-out.temp test-1-single-universe-data/test1_src test-1-single-universe-data/test1_rcv test-1-single-universe-data/src_expected test-1-single-universe-data/rcv_expected 2
 sh testing.sh 2-Two-Uni-Sync 2src-out.temp 2rcv-out.temp test-2-two-uni-sync/src test-2-two-uni-sync/rcv test-2-two-uni-sync/src_expected test-2-two-uni-sync/rcv_expected 2
 sh testing.sh 3-Discover-1-Src 3src-out.temp 3rcv-out.temp test-3-discover-1-src/src test-3-discover-1-src/rcv test-3-discover-1-src/src_expected test-3-discover-1-src/rcv_expected 20
+sh testing.sh 4-Overflow-Sequence 4src-out.temp 4rcv-out.temp test-4-single-universe-data-overflow-sequence/src test-4-single-universe-data-overflow-sequence/rcv test-4-single-universe-data-overflow-sequence/src_expected test-4-single-universe-data-overflow-sequence/rcv_expected 10
