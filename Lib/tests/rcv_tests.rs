cbf2ca8e896f42a6540faa2a92e85ce6d3fba412
Paul Lancaster <paul@lancasterzone.com>
2020-03-16 14:41:37 +0000

    Document formatting messed with comment placement within the test data structures

--- Lib/tests/rcv_tests.rs
+++ Lib/tests/rcv_tests.rs
@@ -21,35 +21,33 @@ use sacn::error::errors::*;
 /// ANSI E1.31-2018 Appendix B Table B-13: Universe Synchronization Example E1.31 Data Packet.
 const TEST_DATA_PACKET: &[u8] = &[
     // Root Layer
-    // Preamble Size
-    0x00, 0x10, // Post-amble Size
-    0x00, 0x00, // ACN Packet Identifier
-    0x41, 0x53, 0x43, 0x2d, 0x45, 0x31, 0x2e, 0x31, 0x37, 0x00, 0x00, 0x00,
-    // Flags and Length Protocol
-    0x72, 0x6e, // Vector
-    0x00, 0x00, 0x00, 0x04, // CID
-    0xef, 0x07, 0xc8, 0xdd, 0x00, 0x64, 0x44, 0x01, 0xa3, 0xa2, 0x45, 0x9e, 0xf8, 0xe6, 0x14, 0x3e,
-    // Data Packet Framing Layer
-    // Flags and Length
-    0x72, 0x58, // Vector
-    0x00, 0x00, 0x00, 0x02, // Source Name
-    b'S', b'o', b'u', b'r', b'c', b'e', b'_', b'A', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0x00, 0x10, // Preamble Size
+    0x00, 0x00, // Post-amble Size
+    0x41, 0x53, 0x43, 0x2d, 0x45, 0x31, 0x2e, 0x31, 0x37, 0x00, 0x00, 0x00, // ACN Packet Identifier
+    0x72, 0x6e, // Flags and Length Protocol
+    0x00, 0x00, 0x00, 0x04, // Vector
+    0xef, 0x07, 0xc8, 0xdd, 0x00, 0x64, 0x44, 0x01, 0xa3, 0xa2, 0x45, 0x9e, 0xf8, 0xe6, 0x14, 0x3e, // CID
+
+    /* Data Packet Framing Layer */
+    0x72, 0x58, // Flags and Length
+    0x00, 0x00, 0x00, 0x02,  // Vector
+    b'S', b'o', b'u', b'r', b'c', b'e', b'_', b'A', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Source Name
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, // Priority
-    100, // Synchronization Address
-    0x1F, 0x1A, // 7962
-    // Sequence Number
-    154, // Options
-    0, // Universe
-    0, 1, // DMP Layer
-    // Flags and Length
-    0x72, 0x0b, // Vector
-    0x02, // Address and Data Type
-    0xa1, // First Property Address
-    0x00, 0x00, // Address Increment
-    0x00, 0x01, // Property value count
-    0x02, 0x01, // Property values
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    100, // Priority
+    0x1F, 0x1A, // Synchronization Address, 7962
+    154, // Sequence Number
+    0, // Options
+    0, 1, // Universe
+
+    /* DMP Layer */
+    0x72, 0x0b, // Flags and Length
+    0x02, // Vector
+    0xa1, // Address and Data Type
+    0x00, 0x00, // First Property Address
+    0x00, 0x01, // Address Increment
+    0x02, 0x01, // Property value count
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Property values
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
