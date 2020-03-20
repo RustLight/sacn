ba7ada1469af70930ed28f1209cfc0f5fc59804a
Paul <paul@lancasterzone.com>
2020-03-20 18:15:47 +0000

    Renamed integration tests, added DROP to receivers

--- Lib/src/recieve.rs
+++ Lib/src/recieve.rs
@@ -43,7 +43,7 @@ use std::borrow::Cow;
 use std::cmp::{max, Ordering};
 use std::collections::HashMap;
 use std::fmt;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, Shutdown};
 use std::time::{Duration, Instant};
 
 /// The default size of the buffer used to recieve E1.31 packets.
@@ -792,6 +792,18 @@ impl SacnReceiver {
     }
 }
 
+/// By implementing the Drop trait for SacnNetworkReceiver it means that the user doesn't have to explicitly cleanup the receiver
+/// and if it goes out of reference it will clean itself up.
+impl Drop for SacnReceiver {
+    fn drop(&mut self){
+        let universes = self.universes.clone();
+        for u in universes{
+            self.mute_universe(u);
+        }
+    }
+}
+
+
 /// Searches for the discovered source with the given name in the given vector of discovered sources and
 /// returns the index of the src in the Vec or None if not found.
 ///
@@ -1100,6 +1112,19 @@ impl SacnNetworkReceiver {
     ///
     pub fn set_timeout(&mut self, timeout: Option<Duration>) -> Result<()> {
         Ok(self.socket.set_read_timeout(timeout)?)
+    }
+}
+
+/// By implementing the Drop trait for SacnNetworkReceiver it means that the user doesn't have to explicitly cleanup the receiver
+/// and if it goes out of reference it will clean itself up.
+impl Drop for SacnNetworkReceiver {
+    fn drop(&mut self){
+        match self.socket.shutdown(Shutdown::Both) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("Failed to correctly shutdown socket receiver socket, {:?}", e);
+            }
+        }
     }
 }
 
