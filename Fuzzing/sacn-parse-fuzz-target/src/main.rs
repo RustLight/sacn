9647f107b311b46fd22f1c318b7a8ea56f804b5a
Paul Lancaster <paul@lancasterzone.com>
2020-04-08 15:04:03 +0100

    Further work on testing section, interoperability tests detailed, section written for fuzz testing

--- Fuzzing/sacn-parse-fuzz-target/src/main.rs
+++ Fuzzing/sacn-parse-fuzz-target/src/main.rs
@@ -6,6 +6,8 @@ use sacn::packet::*;
 
 fn main() {
     fuzz!(|data: &[u8]| {
+            // Key aim is to check that parse does not crash given a wide variety of data. 
+            // The specific error or packet produced is not the aim of these tests.
             let _ = AcnRootLayerProtocol::parse(data);
         }
     );
