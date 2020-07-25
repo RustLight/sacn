168c7407fb5855661d89c7e137d88ff5d54955b7
Paul Lancaster <paul@lancasterzone.com>
2020-07-25 20:47:35 +0100

    Removed extra stuff related to dissertation

--- Code Documentation/implementors/core/cmp/trait.PartialOrd.js
+++
@@ -1,9 +0,0 @@
-(function() {var implementors = {};
-implementors["sacn"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"sacn/receive/struct.UniversePage.html\" title=\"struct sacn::receive::UniversePage\">UniversePage</a>&gt; for <a class=\"struct\" href=\"sacn/receive/struct.UniversePage.html\" title=\"struct sacn::receive::UniversePage\">UniversePage</a>",synthetic:false,types:["sacn::receive::UniversePage"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"sacn/receive/struct.DMXData.html\" title=\"struct sacn::receive::DMXData\">DMXData</a>&gt; for <a class=\"struct\" href=\"sacn/receive/struct.DMXData.html\" title=\"struct sacn::receive::DMXData\">DMXData</a>",synthetic:false,types:["sacn::receive::DMXData"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
