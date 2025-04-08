e4724623420cf2263b06170c0875b88a475d0d2b
Paul Lancaster <paul@lancasterzone.com>
2025-04-08 22:17:51 +0100

    Simple example of creating a sacn recv tool

---
+++ examples/simple-receiver.rs
@@ -0,0 +1,77 @@
+// Copyright 2025 sacn Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+// Simply listens on the given universe and shows the current data.
+
+extern crate sacn;
+extern crate crossterm;
+
+use std::io::{self, Stdout, Write};
+use std::net::SocketAddr;
+use std::time::{Duration, Instant};
+use crossterm::style::Print;
+use crossterm::{
+    execute, queue,
+    cursor, terminal
+};
+
+use sacn::receive::{DMXData, SacnReceiver};
+use sacn::packet::ACN_SDT_MULTICAST_PORT;
+
+fn main() {
+    let interface_ip = "127.0.0.1";
+    let universe = 1;
+    let duration = Duration::from_secs(12);
+
+    let mut dmx_recv = SacnReceiver::with_ip(SocketAddr::new(interface_ip.parse().unwrap(), ACN_SDT_MULTICAST_PORT), None).unwrap();
+    dmx_recv.listen_universes(&[universe]).unwrap();
+
+    println!("Started");
+
+    let start = Instant::now();
+    let mut remaining = duration.checked_sub(start.elapsed()).unwrap_or(Duration::from_millis(0));
+
+    let mut stdout = io::stdout();
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+    while remaining > Duration::from_millis(250) {
+        match dmx_recv.recv(Some(remaining)) {
+            Ok(data) => {
+                display_data(&mut stdout, &data[0]);
+            }
+            Err(e) => {
+                execute!(stdout, cursor::MoveTo(0, 0), terminal::Clear(terminal::ClearType::All)).unwrap();
+                println!("{}", e);
+            },
+        }
+        remaining = duration.checked_sub(start.elapsed()).unwrap_or(Duration::from_millis(0));
+    }
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+}
+
+fn display_data(stdout: &mut Stdout, data: &DMXData) {
+    // Don't worry about this bit - its just for actually displaying the data
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+    for y in 0..16 {
+        queue!(
+            stdout,
+            cursor::MoveTo(0, y),
+            Print("|")
+        ).unwrap();
+        for x in 0..32 {
+            let data_index = ((y * 16 + x) + 1) as usize;
+            let cursor_x = x * 4 + 1;
+            queue!(
+                stdout,
+                cursor::MoveTo(cursor_x, y),
+                Print(
+                    format!("{:03}|", data.values[data_index])
+                )
+            ).unwrap();
+        }
+    }
+    stdout.flush().unwrap();
+}
